b16d02b4cc36d2f2846e817519737f04b145626b
shiwanikumari123 <33774158+shiwanikumari123@users.noreply.github.com>
2021-05-26 15:23:25 +0530

    changes done
    changes done

---
+++ prog6.js
@@ -0,0 +1,16 @@
+function greet() {
+    return 'Hello';
+}
+
+
+function name(user, func)
+{
+
+    const message = func();
+
+    console.log(`${message} ${user}`);
+}
+
+name('John', greet);
+name('Jack', greet);
+name('Sara', greet);
---
+++ prog8.js
@@ -0,0 +1,14 @@
+
+for(var x=20; x<=30; x++) 
+{
+        if (x % 2 == 0)
+        {
+                document.write(x +  " is even ");
+                document.write("<br>");
+        }
+        else {
+                document.write(x + " is odd \n");
+                document.write("<br>");
+        }
+}
+
